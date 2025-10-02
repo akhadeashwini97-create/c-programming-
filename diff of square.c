91d9f06cbd3f1d908c78214060930cc3b1ab541f
akhadeashwini97-create <akhadeashwini97@gmail.com>
2025-10-03 00:21:57 +0530

    Add files via upload

---
+++ diff of square.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 9;
+    int b = 5;
+    int result;
+
+    result = (a * a) - (b * b);
+
+    printf("Difference of squares = %d\n", result);
+    return 0;
+}
