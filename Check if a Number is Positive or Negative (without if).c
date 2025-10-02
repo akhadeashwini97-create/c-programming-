ae47868c9f3030cb1635487c1f8dbd0585fad099
akhadeashwini97-create <akhadeashwini97@gmail.com>
2025-10-03 00:33:16 +0530

    Add files via upload

---
+++ Check if a Number is Positive or Negative (without if).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int number = -3;
+    int isPositive = number > 0;
+
+    printf("Positive: %d\n", isPositive); // 1 = true, 0 = false
+    return 0;
+}
---
+++ Multiply Three Numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2, b = 3, c = 4;
+    int product = a * b * c;
+
+    printf("Product = %d\n", product);
+    return 0;
+}
